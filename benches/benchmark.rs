8eed6411c600a82a97f49674d3b8fe4da10dd958
Matthew Kelly <matthew.kelly2@gmail.com>
2024-10-12 08:02:48 -0400

    Boo:  added arbitrary precision... but fails on Windows

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -8,6 +8,7 @@ use rand::distributions::Uniform;
 use rand::prelude::Distribution;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
+use rug::Float;
 
 fn eval_scalar_polynomial(num_samples: usize) {
     let poly_quadratic = Polynomial::new(vec![-5.0, 2.5, 1.0]);
@@ -18,6 +19,24 @@ fn eval_scalar_polynomial(num_samples: usize) {
         black_box(poly_quadratic.eval(angle));
         black_box(poly_sin_cubic.eval(angle));
         black_box(poly_cos_quartic.eval(angle));
+    }
+}
+
+fn eval_high_precision_polynomial(num_samples: usize, precision: u32) {
+    let mut rng = StdRng::seed_from_u64(12345);
+    let dist = Uniform::from(-9.0..9.0);
+    let mut sample_vector = |length: usize| -> Vec<Float> {
+        (0..length)
+            .map(|_| Float::with_value(precision, dist.sample(&mut rng)))
+            .collect()
+    };
+    let poly_3 = Polynomial::new(sample_complex_vector(3));
+    let poly_4 = Polynomial::new(sample_complex_vector(4));
+    let poly_5 = Polynomial::new(sample_complex_vector(5));
+    for angle in queries {
+        black_box(poly_3.eval(value));
+        black_box(poly_4.eval(value));
+        black_box(poly_5.eval(value));
     }
 }
 
@@ -52,6 +71,9 @@ fn benchmark(c: &mut Criterion) {
     });
     c.bench_function("eval_complex_polynomial", |b| {
         b.iter(|| eval_complex_polynomial(5000))
+    });
+    c.bench_function("eval_high_precision_polynomial", |b| {
+        b.iter(|| eval_high_precision_polynomial(5000, 250))
     });
 }
 
