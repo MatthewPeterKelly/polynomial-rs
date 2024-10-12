af19efa4fc315771c7d7c533c0166615787f9417
Matthew Kelly <matthew.kelly2@gmail.com>
2024-10-12 07:48:16 -0400

    Add test for complex values

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -3,10 +3,11 @@ use std::f64::consts::PI;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use iter_num_tools::lin_space;
 use num_complex::Complex;
-use polynomial::Polynomial;use rand::distributions::Uniform;
+use polynomial::Polynomial;
+use rand::distributions::Uniform;
 use rand::prelude::Distribution;
 use rand::rngs::StdRng;
-use rand::{Rng, SeedableRng};
+use rand::SeedableRng;
 
 fn eval_scalar_polynomial(num_samples: usize) {
     let poly_quadratic = Polynomial::new(vec![-5.0, 2.5, 1.0]);
@@ -23,26 +24,19 @@ fn eval_scalar_polynomial(num_samples: usize) {
 fn eval_complex_polynomial(num_samples: usize) {
     let mut rng = StdRng::seed_from_u64(12345);
     let dist = Uniform::from(-9.0..9.0);
-
-    let mut sample_complex_value = ||{Complex::new(dist.sample(&mut rng), dist.sample(&mut rng))};
-
-    let sample_complex_vector = |length: usize| -> Vec<Complex<f64>> {
+    let mut sample_complex_vector = |length: usize| -> Vec<Complex<f64>> {
         (0..length)
-            .map(|_| {
-                let real_part = dist.sample(&mut rng);
-                let imaginary_part = dist.sample(&mut rng);
-                Complex::new(real_part, imaginary_part)
-            })
+            .map(|_| Complex::new(dist.sample(&mut rng), dist.sample(&mut rng)))
             .collect()
     };
 
-    let poly_3 =  Polynomial::new(sample_complex_vector(3));
-    let poly_4 =  Polynomial::new(sample_complex_vector(4));
-    let poly_5 =  Polynomial::new(sample_complex_vector(5));
+    let poly_3 = Polynomial::new(sample_complex_vector(3));
+    let poly_4 = Polynomial::new(sample_complex_vector(4));
+    let poly_5 = Polynomial::new(sample_complex_vector(5));
     let sqrt_sample_count = (num_samples as f64).sqrt() as usize;
-    let real_iter = lin_space(-5.0..=5.0,sqrt_sample_count);
-    let imag_iter = lin_space(-5.0..=5.0,sqrt_sample_count);
-    for real in real_iter  {
+    let real_iter = lin_space(-5.0..=5.0, sqrt_sample_count);
+    for real in real_iter {
+        let imag_iter = lin_space(-5.0..=5.0, sqrt_sample_count);
         for imag in imag_iter {
             let value = Complex::new(real, imag);
             black_box(poly_3.eval(value));
