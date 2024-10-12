0c2fb090404186b43a087783cd74fa5ab4e8b933
Matthew Kelly <matthew.kelly2@gmail.com>
2024-10-12 06:54:41 -0400

    Added better scalar test with more orders

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,20 +1,25 @@
 use std::f64::consts::PI;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use polynomial::Polynomial;
 use iter_num_tools::lin_space;
+use polynomial::Polynomial;
 
-fn eval_cubic_scalar_polynomial() {
-    let poly = Polynomial::chebyshev(&f64::sin, 4, -PI, PI).unwrap();
-    let queries = lin_space(-5.0..=5.0, 100);
+fn eval_scalar_polynomial(num_samples: usize) {
+    let poly_quadratic = Polynomial::new(vec![-5.0, 2.5, 1.0]);
+    let poly_sin_cubic = Polynomial::chebyshev(&f64::sin, 4, -PI, PI).unwrap();
+    let poly_cos_quartic = Polynomial::chebyshev(&f64::cos, 5, -PI, PI).unwrap();
+    let queries = lin_space(-5.0..=5.0, num_samples);
     for angle in queries {
-        let value = poly.eval(angle);
-        black_box(value);
+        black_box(poly_quadratic.eval(angle));
+        black_box(poly_sin_cubic.eval(angle));
+        black_box(poly_cos_quartic.eval(angle));
     }
 }
 
 fn benchmark(c: &mut Criterion) {
-    c.bench_function("eval_cubic_scalar_polynomial", |b| b.iter(eval_cubic_scalar_polynomial));
+    c.bench_function("eval_cubic_scalar_polynomial", |b| {
+        b.iter(|| eval_scalar_polynomial(5000))
+    });
 }
 
 criterion_group!(benches, benchmark);
