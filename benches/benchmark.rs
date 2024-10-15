e8b4945eb87df5abe753d77058d9b443db501cc4
Matthew Kelly <matthew.kelly2@gmail.com>
2024-10-15 18:44:16 -0400

    WIP:  rational

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -11,31 +11,33 @@ use rand::prelude::Distribution;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
 
-fn create_rng_scalar_polynomial<R, D>(num_samples: usize, rng: &mut R, dist: &D) -> Polynomial<f64>
+
+fn create_rng_scalar_polynomial<R, D>(
+    num_samples: usize,
+    rng: &mut R,
+    dist: &D,
+) -> Polynomial<f64>
 where
     R: Rng,
     D: Distribution<f64>,
 {
-    Polynomial::new((0..num_samples).map(|_| dist.sample(rng)).collect())
+    Polynomial::new(
+        (0..num_samples)
+            .map(|_| dist.sample(rng))
+            .collect(),
+    )
 }
 
 fn eval_scalar_polynomial(num_samples: usize, poly_arr: &[Polynomial<f64>; 3]) {
-    let queries = lin_space(-5.0..=5.0, num_samples);
-    for query in queries {
-        for poly in poly_arr {
-            black_box(poly.eval(query));
+        let queries = lin_space(-5.0..=5.0, num_samples);
+        for query in queries {
+            for poly in poly_arr {
+                black_box(poly.eval(query));
+            }
         }
-    }
 }
 
-fn eval_high_precision_polynomial(_num_samples: usize) {
-    let val_1 = BigRational::new(BigInt::from(1), BigInt::from(3));
-    let val_2 = BigRational::new(BigInt::from(2), BigInt::from(3));
-    let val_3 = BigRational::new(BigInt::from(5), BigInt::from(7));
-    let poly_3 = Polynomial::new([val_1, val_2, val_3].to_vec());
 
-    black_box(poly_3.eval(BigRational::new(BigInt::from(3), BigInt::from(4))));
-}
 
 fn create_rng_complex_polynomial<R, D>(
     num_samples: usize,
@@ -66,29 +68,96 @@ fn eval_complex_polynomial(sqrt_num_samples: usize, poly_arr: &[Polynomial<Compl
     }
 }
 
+
+
+fn eval_high_precision_polynomial(_num_samples: usize) {
+    let val_1 = BigRational::new(BigInt::from(1), BigInt::from(3));
+    let val_2 = BigRational::new(BigInt::from(2), BigInt::from(3));
+    let val_3 = BigRational::new(BigInt::from(5), BigInt::from(7));
+    let poly_3 = Polynomial::new([val_1, val_2, val_3].to_vec());
+
+    black_box(poly_3.eval(BigRational::new(BigInt::from(3), BigInt::from(4))));
+}
+
+
+
+
+
+fn create_rng_big_rational_polynomial<R, D>(
+    num_samples: usize,
+    rng: &mut R,
+    dist: &D,
+) -> Polynomial<BigRational>
+where
+    R: Rng,
+    D: Distribution<i64>,
+{
+
+
+
+    Polynomial::new(
+        (0..num_samples)
+            .map(|_|
+            {
+
+                let num =  BigInt::from(dist.sample(rng)) *  BigInt::from(dist.sample(rng));
+                let den =  BigInt::from(dist.sample(rng)) *  BigInt::from(dist.sample(rng));
+let rational = BigRational::new(num, den);
+
+// TODO:  rational into poly
+
+            }
+
+
+
+
+
+            )
+            .collect(),
+    )
+}
+
+fn eval_scalar_polynomial(num_samples: usize, poly_arr: &[Polynomial<f64>; 3]) {
+        let queries = lin_space(-5.0..=5.0, num_samples);
+        for query in queries {
+            for poly in poly_arr {
+                black_box(poly.eval(query));
+            }
+        }
+}
+
+
+
+
+
+
+
+
+
 fn benchmark(c: &mut Criterion) {
     // Set up for all of the benchmarks:
 
     let num_samples = 5000;
     let mut rng = StdRng::seed_from_u64(12345);
-    let real_dist = Uniform::from(-9.0..9.0);
+    let f64_dist = Uniform::from(-9.0..9.0);
+    let i64_dist = Uniform::from(i64::MIN..i64::MAX);
 
-    let scalar_poly_arr = [
-        create_rng_scalar_polynomial(3, &mut rng, &real_dist),
-        create_rng_scalar_polynomial(4, &mut rng, &real_dist),
-        create_rng_scalar_polynomial(5, &mut rng, &real_dist),
+    let scalar_poly_arr= [
+        create_rng_scalar_polynomial(3, &mut rng, &f64_dist),
+        create_rng_scalar_polynomial(4, &mut rng, &f64_dist),
+        create_rng_scalar_polynomial(5, &mut rng, &f64_dist),
     ];
 
     let complex_poly_arr = [
-        create_rng_complex_polynomial(3, &mut rng, &real_dist),
-        create_rng_complex_polynomial(4, &mut rng, &real_dist),
-        create_rng_complex_polynomial(5, &mut rng, &real_dist),
+        create_rng_complex_polynomial(3, &mut rng, &f64_dist),
+        create_rng_complex_polynomial(4, &mut rng, &f64_dist),
+        create_rng_complex_polynomial(5, &mut rng, &f64_dist),
     ];
 
     // Actually run all of the benchmarks:
 
     c.bench_function("eval_scalar_polynomial", |b| {
-        b.iter(|| eval_scalar_polynomial(num_samples, &scalar_poly_arr))
+        b.iter(|| eval_scalar_polynomial(num_samples , &scalar_poly_arr))
     });
 
     c.bench_function("eval_complex_polynomial", |b| {
